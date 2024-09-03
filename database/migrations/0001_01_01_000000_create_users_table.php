e8a780fc64243297dc8e8d29c0d4a3da039e5cc8
ahilarion <alan.hilarion@proton.me>
2024-09-03 16:58:20 +0200

    fix

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -34,7 +34,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
         Schema::dropIfExists('sessions');
+        Schema::dropIfExists('users');
     }
 };
