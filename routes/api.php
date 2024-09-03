96156036c9dc391789163b5c58d6931be8e2e3b8
ahilarion <alan.hilarion@proton.me>
2024-09-03 14:55:53 +0200

    fix

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
 
-    Route::middleware('auth:web')->group(function () {
+    Route::middleware('auth:sanctum')->group(function () {
         Route::post('logout', [AuthController::class, 'logout']);
         Route::post('me', [AuthController::class, 'me']);
     });
